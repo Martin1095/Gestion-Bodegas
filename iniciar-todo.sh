e8d5d31c8ddcaf32036c086309b9ac9c96418665
Martin1095 <mart.maulen@duocuc.cl>
2026-06-19 22:56:34 -0400

    Implementacion de eureka y cambios de los pom

--- iniciar-todo.sh
+++ iniciar-todo.sh
@@ -9,34 +9,34 @@ sleep 12
 echo "Iniciando API Gateway..."
 osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/gateway\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Jedis..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/jedis\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Articulos..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/articulos\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Bodegas..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/bodegas\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Clientes..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/clientes\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Despachos..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/despachos\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Detalle_pedido..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/detalle_pedidos\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Inventarios..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/inventarios\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Pedidos..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/pedidos\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Proveedores..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/proveedores\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Recepciones..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/recepciones\" && ./mvnw spring-boot:run"'
 
-echo "Iniciando Microservicio Sables..."
-osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/sables\" && ./mvnw spring-boot:run"'
+echo "Iniciando Microservicio Trabajadores..."
+osascript -e 'tell application "Terminal" to do script "cd \"'"$(pwd)"'/trabajadores\" && ./mvnw spring-boot:run"'
 
 echo "Ecosistema lanzado. Dashboard disponible en http://localhost:8761"
